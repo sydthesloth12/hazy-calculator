02cfc0fac69dbca5dc31ad37d4e7bac53028538a
Sydney <sydneysirois17@gmail.com>
2022-04-19 19:00:31 -0400

    comment outline

---
+++ index.js
@@ -0,0 +1,12 @@
+/* eslint-disable spaced-comment */
+const calculate = () => {
+  //calculates the given array of values
+  //`'2'` is treated as a number
+  // `'NULL'` is treated as zero
+  //undefined is ignored
+  //empty string is ignored
+  //non-numeric values are ignored
+  //returns the value as a number
+  
+
+}
