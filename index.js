474f4f005116dc35d512a083f2391b4b372e3a2d
Sydney <sydneysirois17@gmail.com>
2022-04-23 12:55:43 -0400

    something, but its wrong

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
 /* eslint-disable spaced-comment */
-const zeroValue = (value) => {
-  return 0
-}
+const getValues = (array) => {
+  let newArray = []
+  let operation
+  let number
 
-const nothingValue = (value) => {
-  return value === null
-}
+  for (let i = 0; i < array.length; i++) {
+    const currentItem = array[i]
 
-const acceptableOperator = (value) => {
-  const operators = ['+', '-', '*', '/']
+    if (currentItem !== NaN) {
+      let number = Number(currentItem)
 
-  return typeof value === Number || operators.includes(value)
-}
+      currentItem.push(newArray)
+    } if (currentItem === '+' || '-' || '*' || '/') {
+      let currentItem = operation
 
-const calculate = (operandOne, operator, operandTwo) => {
-  switch (operator) {
-    case '+': 
-      return operandOne + operandTwo
-    case '-':
-      return operandOne - operandTwo
-    case '*':
-      return operandOne * operandTwo
-    case '/': 
-      return operandOne / operandTwo
-    default: 
-      return "Invalid input"
-
+      operation.push(newArray)
+    }
   }
 }
+
+const testArray = ['2', '*', 4]
+
+console.log(getValues(testArray))
+
+
+
+
+// const performCalc = (array) => {
+//   switch (array) {
+//     case '+':
+//       return operandOne + operandTwo
+//     case '-':
+//       return operandOne - operandTwo
+//     case '*':
+//       return operandOne * operandTwo
+//     case '/':
+//       return operandOne / operandTwo
+//     default:
+//       return 'Invalid input'
+//  
+
+// const test = ['2', '+', '10']
+
+// console.log(performCalc(test))
+
+
+//write a function that loops through an array, evaluates each value of that array, and only pulls out
+//string of numbers and operators
+//takes those values and pushes them into a new array
+
+//calculate function grabs the values from the new array, turns the string value to number,
+//uses the operator to perform calculation
 
 //loop through the array that is given
 //ignore undefined, empty string values, and non-numeric values
@@ -36,3 +59,5 @@ const calculate = (operandOne, operator, operandTwo) => {
 //find the operand in the array, utilize that later to do the math portion of it 
 
 //grab numeric values, whether string or not, and turn them into numbers to utilize for equations  
+
+module.exports = calculate
