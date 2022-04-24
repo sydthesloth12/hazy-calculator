f28247c574957b597599aac06dabcb2a0a2dc306
Sydney <sydneysirois17@gmail.com>
2022-04-24 19:59:12 -0400

    progress

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-const calculate = (array) => {
-  
+/* eslint-disable spaced-comment */
+const getValues = (array) => {
+  let newArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    const currentItem = array[i]
+
+  if (isNaN(currentItem) === false) {
+    newArray.push(currentItem)
+  } if (currentItem === '+') {
+    newArray.push(currentItem)
+  } if (currentItem === '-') {
+    newArray.push(currentItem)
+  } if (currentItem === '*') {
+    newArray.push(currentItem)
+  } if (currentItem === '/') {
+    newArray.push(currentItem)
+  } if (currentItem === null) {
+    let currentItem = 0
+    newArray.push(currentItem)
+  } else {
+    
+  }
 }
+}
+
+const testArray = ['foo', 3, null, '*']
+const test = getValues(testArray)
+
+console.log(test)
+// const performCalc = (array) => {
+//   switch (array) {
+//     case '+':
+//       return operandOne + operandTwo
+//     case '-':
+//       return operandOne - operandTwo
+//     case '*':
+//       return operandOne * operandTwo
+//     case '/':
+//       return operandOne / operandTwo
+//     default:
+//       return 'Invalid input'
+//   }
+
+
+
+
